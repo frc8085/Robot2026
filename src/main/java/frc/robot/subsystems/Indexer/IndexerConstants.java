55883707c85081a0955b362a1a7a371573f5f377
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2026-01-23 18:02:17 -0500

    Added kicker to subsystem

--- src/main/java/frc/robot/subsystems/Indexer/IndexerConstants.java
+++ src/main/java/frc/robot/subsystems/Indexer/IndexerConstants.java
@@ -2,4 +2,5 @@ package frc.robot.subsystems.Indexer;
 
 public class IndexerConstants {
     public static final int CanIndexer = 23;
+    public static final int CanKicker = 24;
 }
--- src/main/java/frc/robot/subsystems/Indexer/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/Indexer/IndexerSubsystem.java
@@ -5,12 +5,18 @@ import frc.robot.lib.TalonFXMotor;
 
 public class IndexerSubsystem{
     private PIDMotor indexerMotor;
+    private PIDMotor kickerMotor;
 
     public IndexerSubsystem(){
         this.indexerMotor = new TalonFXMotor(IndexerConstants.CanIndexer);
+        this.kickerMotor = new TalonFXMotor(IndexerConstants.CanKicker);
     }
 
-    public void setSpeed(double speed){
+    public void setIndexerSpeed(double speed){
         this.indexerMotor.setSpeed(speed);
+    }
+
+    public void setKickerSpeed(double speed){
+        this.kickerMotor.setSpeed(speed);
     }
 }
