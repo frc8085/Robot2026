6ca896cab305f383c9743f126ab3a0bec2eeda78
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2026-01-31 11:19:35 -0500

    added PID values to match drive for now

--- src/main/java/frc/robot/subsystems/Flywheel/FlywheelConstants.java
+++ src/main/java/frc/robot/subsystems/Flywheel/FlywheelConstants.java
@@ -8,12 +8,12 @@ public final class FlywheelConstants {
     public static final double kFlywheelToleranceMPS = 0.5; // Acceptable tolerance for RPM
     public static final double kFlywheelRadius = .0508; // radius in meters (2 inches)
 
-    public static final double kP = 0.1; // Proportional gain for velocity control
+    public static final double kP = 0.11; // Proportional gain for velocity control
     public static final double kI = 0.0; // Integral gain for velocity control  
     public static final double kD = 0.0; // Derivative gain for velocity control
-    public static final double kV = 0.0002; // Velocity feedforward gain
-    public static final double kA = 0.0001; // Acceleration feedforward gain
-    public static final double kS = 0.05; // Static feedforward gain
+    public static final double kV = 0.12; // Velocity feedforward gain
+    public static final double kA = 0.01; // Acceleration feedforward gain
+    public static final double kS = 0.20; // Static feedforward gain
 
     public static final double kStatorCurrentLimit = 120;
     public static final double kSupplyCurrentLimit = 70;
