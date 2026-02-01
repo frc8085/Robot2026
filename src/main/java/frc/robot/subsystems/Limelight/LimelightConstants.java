84e4ebbf38a40e35de5e2e6d4ca819449a6c5f3f
lseluga <loriann.higashi@yale.edu>
2026-02-01 18:37:37 -0500

    Update LimelightConstants.java

--- src/main/java/frc/robot/subsystems/Limelight/LimelightConstants.java
+++ src/main/java/frc/robot/subsystems/Limelight/LimelightConstants.java
@@ -2,18 +2,18 @@ package frc.robot.subsystems.Limelight;
 
 public final class LimelightConstants {
 
-    public static final double limelightBlueForward = -0.356;
-    public static final double limelightBlueSide = -0.3302; // old: -0.0254
-    public static final double limelightBlueUp = 0.248;
+    public static final double limelightBlueForward = -0.2921;
+    public static final double limelightBlueSide = -0.1524; // old: -0.0254
+    public static final double limelightBlueUp = 0.254;
     public static final double limelightBlueRoll = 0;
     public static final double limelightBluePitch = 0;
     public static final double limelightBlueYaw = 70;
 
-    public static final double limelightYellowForward = -0.356;
-    public static final double limelightYellowSide = 0.3302; // old: 0.0254
-    public static final double limelightYellowUp = 0.248;
+    public static final double limelightYellowForward = 0.33;
+    public static final double limelightYellowSide = 0; // old: 0.0254
+    public static final double limelightYellowUp = 0.2921;
     public static final double limelightYellowRoll = 0;
-    public static final double limelightYellowPitch = 0;
-    public static final double limelightYellowYaw = -70;
+    public static final double limelightYellowPitch = 30;
+    public static final double limelightYellowYaw = 30;
 
 }
